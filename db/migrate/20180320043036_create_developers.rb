f2c63ef192943f104adcdc8ec2d6f15ca9eca214
Hoa Vo <hoa.vo@tech.reapra.sg>
2018-03-20 13:47:00 +0700

    Add Developer

---
+++ db/migrate/20180320043036_create_developers.rb
@@ -0,0 +1,10 @@
+class CreateDevelopers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :developers do |t|
+      t.string :email, null: false
+
+      t.timestamps
+    end
+    add_index :developers, :email, unique: true
+  end
+end
