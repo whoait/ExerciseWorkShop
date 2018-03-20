82b68b7c8710745152c3338f438c216840e892b5
Hoa Vo <hoa.vo@tech.reapra.sg>
2018-03-20 14:48:11 +0700

    Add relationship of  Developer and ProgrammingLanguage

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,2 +1,4 @@
 class Developer < ApplicationRecord
+  has_many :developer_programmings, dependent: :destroy
+  has_many :programming_language, through: :developer_programmings
 end
--- app/models/programming_language.rb
+++ app/models/programming_language.rb
@@ -1,2 +1,4 @@
 class ProgrammingLanguage < ApplicationRecord
+  has_many :developer_programmings, dependent: :destroy
+  has_many :developer, through: :developer_programmings
 end
