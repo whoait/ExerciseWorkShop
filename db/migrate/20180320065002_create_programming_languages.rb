bb19d23222224747ac9563bd2e8c7a9394eb95c8
Hoa Vo <hoa.vo@tech.reapra.sg>
2018-03-20 13:52:53 +0700

    Add ProgrammingLanguage

---
+++ db/migrate/20180320065002_create_programming_languages.rb
@@ -0,0 +1,10 @@
+class CreateProgrammingLanguages < ActiveRecord::Migration[5.1]
+  def change
+    create_table :programming_languages do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+    add_index :programming_languages, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320043036) do
+ActiveRecord::Schema.define(version: 20180320065002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20180320043036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_developers_on_email", unique: true
+  end
+
+  create_table "programming_languages", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_programming_languages_on_name", unique: true
   end
 
 end
